84db0ef5d0813b3ca9695e53f9cac0175da74922
Prince Kumar <dhimanprince262@gmail.com>
2026-04-04 19:06:42 +0530

    grid1 and grid2 made in about me section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from './section/Navbar.jsx'
 import Hero from './section/Hero.jsx'
+import About from './section/About.jsx'
 
 const App = () => {
   return (
     <div className='container max-w-7xl mx-auto'>
       <Navbar />
       <Hero />
+      <About />
 
       <section className='min-h-screen'/>
       <section className='min-h-screen'/>
